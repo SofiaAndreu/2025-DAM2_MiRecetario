66e7089d72eb77aefaa0a73996170bad0187dba9
SofiaAndreu <sofia.andreu2004@gmail.com>
2025-10-30 19:45:10 +0100

    correcciones + comentarios

--- TiendaMascotasYDrawer/app/src/main/java/com/example/tiendamascotas/ApiConfig.java
+++
@@ -1,7 +0,0 @@
-package com.example.tiendamascotas;
-
-public class ApiConfig {
-    //DOMINIO
-    public static final String BASE_URL = "https://sandreu.colexio-karbo.com/2025-26/tiendaMascotas/";
-    public static final String API_PRODUCTOS = BASE_URL + "api/productos.php";
-}
